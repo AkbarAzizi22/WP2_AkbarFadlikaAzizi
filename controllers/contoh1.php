8b8b91e3f4c931e475f9b0d59de46c0dbef9ff14
akbarfadlikaazizi <akbarfadliazizi@gmail.com>
2024-03-25 22:27:46 +0700

    first commit

---
+++ controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan</h1>";
+        echo"Nama saya Akbar Fadlika Azizi 
+        Saya tinggal di daerah Bekasi 
+        olah raga yang saya sukai adalah tenis meja";
+    }
+}
